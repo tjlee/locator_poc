2328c338321178d39d3caf960aefe09f4a3c3080
Chernov Vasilii <v.chernov@crystals.ru>
2015-03-04 17:27:17 +0300

    initial commit

---
+++ source/tmp.py
@@ -0,0 +1,26 @@
+__author__ = 'v.chernov'
+
+
+# here we are ... we need to store the following data
+# todo: data structure
+# {
+#     userId: "",
+#     dateTime: "",
+#     location: {
+#         from: {
+#             x: "",
+#             y: "",
+#             name: ""
+#             },
+#         to:  {
+#             x: "",
+#             y: "",
+#             name: ""
+#             }
+#     }
+#     places: ""
+# }
+
+# todo: store in mongodb
+
+# todo: search from predefined place to another predefined place
